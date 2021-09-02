cb7cca3c6f35931ce42f203ccccaa7b2cbf4a916
02032003 <89027022+02032003@users.noreply.github.com>
2021-09-02 08:47:42 -0700

    Update exercise.py

--- assignments/01MultiplicacionRusa/src/exercise.py
+++ assignments/01MultiplicacionRusa/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
-    pass
+fac1=int(input())
+fac2=int(input())
+cont=0
+
+if(fac1>0):
+    if(fac2%2!=0):
+        cont=cont+fac1
+        print(fac1,fac2)
+    else:
+        print(fac1,fac2)
+    while(fac2!=1):
+        fac1*=2
+        fac2=fac2//2
+        if(fac2%2!=0):
+            cont=cont+fac1
+        print(fac1,fac2)
+    print(cont)
+else:
+    print(0)
 
 
 if __name__=='__main__':
